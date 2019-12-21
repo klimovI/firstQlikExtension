9b63ce2c58a9bb5c842ad80e6ad88e8d9cb3e508
klimovI <irracli@gmail.com>
2019-12-21 17:47:45 +0800

    added initialproperties and definition

--- src/qwe.js
+++ src/qwe.js
@@ -1,7 +1,11 @@
+import initialProperties from './initialProperties.json';
+import definition from './definition';
 import template from './template/template.html';
 import paint from './paint';
 
 export default {
+  initialProperties,
+  definition,
   support: {
     snapshot: true,
     export: true,
